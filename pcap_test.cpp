7f13324a841ec36d2d7aa5e2c6e4eabb2b134a72
lawheon <lawheon@naver.com>
2017-07-16 10:44:09 -0400

    Add pcap_test.cpp

---
+++ pcap_test.cpp
@@ -0,0 +1,77 @@
+
+#include <stdio.h>
+#include <pcap.h>
+#include <stdlib.h>
+
+int main()
+{
+	pcap_t *handle;
+	char *dev;
+	char errbuf[PCAP_ERRBUF_SIZE];
+	struct bpf_program fp;
+	char filter_exp[] = "port 80";
+	bpf_u_int32 mask;
+	bpf_u_int32 net;
+	struct pcap_pkthdr *header;
+	const u_char *packet;
+
+	/*Define the device */
+	dev = pcap_lookupdev(errbuf);
+	if(dev == NULL) {
+		fprintf(stderr, "Couldn't find default device: %s\n", errbuf);
+		return (2);
+	}
+
+	/*Find the properties for the device */
+	if (pcap_lookupnet(dev, &net, &mask, errbuf) == -1) {
+		fprintf(stderr, "Couldn't get netmask for device %s: %s\n", dev, errbuf);
+		net = 0;
+		mask= 0;
+	}
+
+	/*Open the session in promiscuous mod */
+	handle = pcap_open_live(dev, BUFSIZ, 1, 1000, errbuf);
+	if (handle == NULL) {
+		fprintf(stderr, "Couldn't open device %s: %s\n", dev, errbuf);
+		return 0;
+	}
+
+	/*Compile and apply the filter */
+	if (pcap_compile(handle, &fp, filter_exp, 0, net) == -1) {
+		fprintf(stderr, "Couldn't parse filter %s: %s\n", filter_exp, pcap_geterr(handle));
+		return(2);
+		}
+
+		if (pcap_setfilter(handle, &fp) == -1) {
+		fprintf(stderr, "Couldn't install filter %s: %s\n", filter_exp, pcap_geterr(handle));
+		return 0;
+		}
+		/*Grab a packet */
+		pcap_next_ex(handle, &header, &packet);
+
+		/*Print */
+		printf("Packet Capture %d bytes\n", header->len);
+		for (int i = 0; i < header->len; i++)
+		{
+			if ((*(packet + i) & 0xff) >= 0x10)
+			{
+				printf("%x ", *(packet + i) & 0xff);
+			}
+			else
+			{
+				printf("0%x ", *(packet + i) & 0xff);
+			}
+			if (i % 16 == 15)
+			{
+				printf("\n");
+			}
+			else if (i % 8 == 7)
+			{
+				printf(" ");
+			}
+		}
+		/*Close */
+		printf("\n");
+		pcap_close(handle);
+		return(0);
+	}
